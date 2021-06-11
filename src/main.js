52c9847d3e6ddfab1ba31926e13b361e69cf64d7
Daine Wright <wrightdm@ornl.gov>
2021-06-11 16:06:15 -0400

    add bootstrap 5

--- src/main.js
+++ src/main.js
@@ -1,4 +1,8 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+const Vue = createApp(App)
+
+import 'bootstrap/dist/css/bootstrap.css'
+
+Vue.mount('#app')
