f5228d9a51afd08f86d82fbe37ba541d84ae532c
Daine Wright <wrightdm@ornl.gov>
2021-06-28 09:39:00 -0400

    add policy section

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,26 @@
 // App.vue
 
 <template>
-    <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-      <div class="container-lg">
-          <a class="navbar-brand nasa">Earthdata Pub Overview</a>
-      </div>
+  <nav class="navbar navbar-expand-lg navbar-dark bg-primary">
+    <div class="container-lg">
+      <a class="navbar-brand nasa">Earthdata Pub Overview</a>
+    </div>
   </nav>
   <div id="app">
     <Daacs />
+    <Policy />
   </div>
 </template>
 
 <script>
-import Daacs from './components/Daacs.vue'
+import Daacs from "./components/Daacs.vue";
+import Policy from "./components/Policy.vue";
 
 export default {
-  name: 'app',
+  name: "app",
   components: {
-    Daacs
-  }
-}
+    Daacs,
+    Policy,
+  },
+};
 </script>
---
+++ src/components/Policy.vue
@@ -0,0 +1,31 @@
+// Policy.vue
+
+<template>
+  <div class="container">
+    <div v-for="policyData in policyDataList" :key="policyData.id" class="container-lg">
+      <div class="row pt-5 justify-content-center">
+        <h2 class="display-4">{{policyData.heading}}</h2>
+        <p v-for="(value, index) in policyData.paragraphs" :key="index">{{value}}</p>
+      </div>
+      <ul>
+        <li v-for="(item, index) in policyData.list" :key="index">{{item}}</li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Policy",
+  data() {
+    return {
+      policyDataList: []
+    };
+  },
+  mounted() {
+    fetch("policy.json")
+      .then(response => response.json())
+      .then(data => (this.policyDataList = data));
+  }
+};
+</script>
