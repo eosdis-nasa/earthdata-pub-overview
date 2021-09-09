117e56bb134e7729598fe07484bf3ab74a66f902
Daine Wright <wrightdm@ornl.gov>
2021-09-09 13:24:58 -0400

    add known bugs page to overview

---
+++ src/components/testing/KnownBugs.vue
@@ -0,0 +1,11 @@
+<template>
+  <div class="container">
+    <div class="container-lg">
+      <div class="pt-5 justify-content-center">
+        <h2 class="display-4">Known Bugs</h2>
+        <p>This is a list of known bugs. If you encounter one of these bugs, feel free to provide feedback on that bug.</p>
+        <iframe width="900" height="500" src="https://publish.smartsheet.com/94b7b79080ca462da4a3668928ab7276"></iframe>
+      </div>
+    </div>
+  </div>
+</template>
