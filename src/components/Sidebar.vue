8a73421f19c2e2019fe99dc823fe7b7795157717
Kimberly Broughton <broughtonkk@ornl.gov>
2023-06-08 15:36:26 -0400

    css tweaks still

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -39,7 +39,7 @@ export default {
     }
   },
   mounted() {
-    localStorage.removeItem('overview-version')
+    localStorage.setItem('overview-version','2')
   },
   methods: {
     toggleCollapseExpand (){
